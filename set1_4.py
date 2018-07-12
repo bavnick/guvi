eafb6a93e83eb80824238eea952b5d4a9bca4390
bavnick <39819770+bavnick@users.noreply.github.com>
2018-07-12 11:27:39 +0400

    Update set1_4.py

--- set1_4.py
+++ set1_4.py
@@ -2,7 +2,7 @@ ip = raw_input()
 if len(ip) > 1:
     print 'Invalid Input'
 else:
-    if ip >= 'a' and ip <= 'z':
+    if (ip >= 'a' and ip <= 'z') or (ip >= 'A' and ip <= 'Z'):
         print 'Alphabet'
     else:
         print 'No'
